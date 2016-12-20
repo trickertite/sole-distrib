4cc9e0a1f3f5dd31c9f17f7464852e8a010d5a3e
hassan-ali-10p <hassan.ali@tenpearls.com>
2016-12-20 14:40:32 +0500

    cucumber feature written

---
+++ features/product/create.feature
@@ -0,0 +1,11 @@
+Feature: Create a product
+As a SoleDistributor
+I want to store a product in the app
+So that I don't have to manually maintain product specs.
+
+  Scenario: creating with valid attributes
+    Given I go to create product page
+    When I create the following product
+      | name       | category | price |
+      | samsung J5 | mobile   | 22000 |
+    Then I should see the message 'product is successfully created'
---
+++ features/step_definitions/product_steps.rb
@@ -0,0 +1,12 @@
+Given(/^I go to create product page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I create the following product$/) do |table|
+  # table is a Cucumber::MultilineArgument::DataTable
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should see the message 'product is successfully created'$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
