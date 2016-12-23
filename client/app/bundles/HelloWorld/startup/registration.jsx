576be9ea7e16da3836b91831e6c4e76ff61538eb
trickertite <trickertite@gmail.com>
2016-12-23 18:04:15 +0500

    Hello_World component removed

--- client/app/bundles/HelloWorld/startup/registration.jsx
+++ client/app/bundles/HelloWorld/startup/registration.jsx
@@ -1,12 +1,10 @@
 import ReactOnRails from 'react-on-rails';
 
-import HelloWorld from '../components/HelloWorld';
 import Main from '../components/Main';
 import CreateProduct from '../components/CreateProduct';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
-  HelloWorld,
   Main,
   CreateProduct,
 });
