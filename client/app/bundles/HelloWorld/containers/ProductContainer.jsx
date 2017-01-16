212b104deec228a2c25f293a8d0ab4b1294778b9
trickertite <trickertite@gmail.com>
2017-01-16 15:56:20 +0500

    ProductContainer component is working

--- client/app/bundles/HelloWorld/containers/ProductContainer.jsx
+++ client/app/bundles/HelloWorld/containers/ProductContainer.jsx
@@ -1,8 +1,18 @@
 // Simple example of a React "smart" component
-
+import React from 'react';
 import { connect } from 'react-redux';
-import Products from '../components/Products';
+// import Products from '../components/Products';
 // import * as actions from '../actions/helloWorldActionCreators';
+class ProductContainer extends React.PureComponent {
+  render () {
+    return (
+      // <ul className="products list-group">
+      //   {productListItems}
+      // </ul>
+      <div>Hello hassan</div>
+    )
+  }
+}
 
 // Which part of the Redux global state does our component want to receive as props?
 const mapStateToProps = (state) => ({ products: state.products });
@@ -10,4 +20,4 @@ const mapStateToProps = (state) => ({ products: state.products });
 // Don't forget to actually use connect!
 // Note that we don't export HelloWorld, but the redux "connected" version of it.
 // See https://github.com/reactjs/react-redux/blob/master/docs/api.md#examples
-export default connect(mapStateToProps)(Products);
+export default connect(mapStateToProps)(ProductContainer);
