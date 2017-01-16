43f6c3c0915cf09831932a035d5cf9ecce001faa
trickertite <trickertite@gmail.com>
2017-01-16 16:46:24 +0500

    components changed to redux plus Main-Component removed

--- client/app/bundles/HelloWorld/components/Products.jsx
+++ client/app/bundles/HelloWorld/components/Products.jsx
@@ -1,44 +1,17 @@
 import React, { PropTypes } from 'react';
 import Product from './Product';
 
-// export default class Products extends React.Component {
-
-//   constructor() {
-//     super();
-//     this.state = {
-//       products: []
-//     }
-//   }
-
-// componentDidMount() { $.getJSON('/products', (response) => { 
-//   this.setState({ producproductReducerts: response }) });
-// }
-
-//   render () {
-//     let productListItems = this.state.products.map((product, index) => {
-//       return <Product product={{name: product.name}} key={index} />;
-//     });
-
-//     return (
-//       <ul className="products list-group">
-//         {productListItems}
-//       </ul>
-//     )
-//   }
-// }
-
 export default class Products extends React.Component {
-
   render () {
     let productListItems = this.props.products.map((product, index) => {
       return <Product product={{name: product.name}} key={index} />;
     });
 
     return (
-      // <ul className="products list-group">
-      //   {productListItems}
-      // </ul>
-      <div>Hello hassan</div>
+      <ul className="products list-group">
+        {productListItems}
+      </ul>
     )
   }
 }
+
--- client/app/bundles/HelloWorld/containers/ProductContainer.jsx
+++ client/app/bundles/HelloWorld/containers/ProductContainer.jsx
@@ -1,15 +1,37 @@
 // Simple example of a React "smart" component
 import React from 'react';
 import { connect } from 'react-redux';
-// import Products from '../components/Products';
+import Products from '../components/Products';
+import CreateProduct from '../components/CreateProduct';
 // import * as actions from '../actions/helloWorldActionCreators';
 class ProductContainer extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showCreateProductFormComp: false,
+    };
+    this._onButtonClick = this._onButtonClick.bind(this);
+  }
+
+  _onButtonClick() {
+    this.setState({showCreateProductFormComp: true});
+  }
+
   render () {
     return (
-      // <ul className="products list-group">
-      //   {productListItems}
-      // </ul>
-      <div>Hello hassan</div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <Products products={this.props.products} />
+            <br/>
+            <button onClick={this._onButtonClick} className="btn btn-primary">Create Product</button>
+            {this.state.showCreateProductFormComp ?
+               <CreateProduct /> :
+               null
+            }
+          </div>
+        </div>
+      </div>
     )
   }
 }
