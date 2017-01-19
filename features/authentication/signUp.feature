6522d4dfbfa4e80762591999f1d7c375141f193b
trickertite <trickertite@gmail.com>
2017-01-19 12:42:05 +0500

    signup feature, scenario and step definations(pending) added

---
+++ features/authentication/signUp.feature
@@ -0,0 +1,10 @@
+Feature: Sign up for user account
+  As a User
+  I want to sign up for an account
+  So that I can access the features of this app
+
+  Scenario: Sign Up with valid attributes
+    Given I go to App Admin
+    When I ask him to create an account
+    Then my account should be created
+    And I should be given my credentials
---
+++ features/step_definitions/authentication_steps.rb
@@ -0,0 +1,15 @@
+Given(/^I go to App Admin$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I ask him to create an account$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^my account should be created$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should be given my credentials$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
