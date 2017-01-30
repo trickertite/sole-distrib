93df5a0e44994fa8e94b0d90f3cad1b53e84567d
trickertite <trickertite@gmail.com>
2017-01-30 12:37:15 +0500

    knock gem integerate & email, password attribs added to users table

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'pg', '~> 0.18.4'
 gem 'react_on_rails', '~> 6.3', '>= 6.3.2'
 gem 'bootstrap', '~> 4.0.0.alpha3'
+gem 'knock', '~> 2.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170120055723) do
+ActiveRecord::Schema.define(version: 20170130072314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,8 +25,11 @@ ActiveRecord::Schema.define(version: 20170120055723) do
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password"
+    t.string   "password_digest"
+    t.string   "email"
   end
 
 end
