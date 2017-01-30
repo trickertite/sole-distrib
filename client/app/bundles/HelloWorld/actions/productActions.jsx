0a5b29dd5b1c5809a0e4c2a33e4ee993f1c996e3
trickertite <trickertite@gmail.com>
2017-01-30 12:52:56 +0500

    authentiaction added to products-index action and authorization header added in react app

--- client/app/bundles/HelloWorld/actions/productActions.jsx
+++ client/app/bundles/HelloWorld/actions/productActions.jsx
@@ -2,5 +2,11 @@ import axios from 'axios';
 
 export const fetchProducts = () => ({
   type: 'FETCH_PRODUCTS',
-  payload: axios.get('/products'),
+  payload: axios({
+    method: 'get',
+    url: '/products',
+    headers: {
+      Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE0ODU4NDkwMzAsInN1YiI6Mn0.TOGH7DcIbMMGP0Z5smxxXMsRzOWXvlja-t9zwSgoZdA',
+    },
+  }),
 });
