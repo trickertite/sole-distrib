d445884e3d972c18eb1924f2931dbf315a709c03
trickertite <trickertite@gmail.com>
2017-02-02 12:42:15 +0500

    eslint fixes

--- client/app/bundles/HelloWorld/actions/authActions.jsx
+++ client/app/bundles/HelloWorld/actions/authActions.jsx
@@ -1,4 +1,3 @@
-// import api from 'utils/api';
 import jwtDecode from 'jwt-decode';
 import axios from 'axios';
 import localStorage from 'local-storage';
@@ -12,7 +11,6 @@ export const login = (credentials) => {
     };
 
     axios.post('/user_token', authHash).then((res) => {
-      console.log(res.data.jwt);
       localStorage.set('authToken', res.data.jwt);
 
       dispatch({
@@ -29,7 +27,7 @@ export const login = (credentials) => {
 };
 
 export const signUp = (user) => {
-  return (dispatch) => {
+  return () => {
     axios.post('/users', user).then((res) => {
       return res;
     }).catch((error) => {
@@ -37,107 +35,3 @@ export const signUp = (user) => {
     });
   };
 };
-
-// import axios from 'axios';
-// import cookie from 'react-cookie';
-// import { AUTH_USER,
-//          AUTH_ERROR,
-//          UNAUTH_USER,
-//          PROTECTED_TEST } from '../constants/authConstants';
-
-// const API_URL = 'http://localhost:3000/api';
-
-// // export const signUpUser = (user) => ({
-// //   type: 'SIGN_UP_USER',
-// //   payload: axios.post('/auth/signUp', user)
-// // });
-
-// export function logoutUser() {
-//   return function logout(dispatch) {
-//     dispatch({ type: UNAUTH_USER });
-//     cookie.remove('token', { path: '/' });
-
-//     // window.location.href = CLIENT_ROOT_URL + '/login';
-//   };
-// }
-
-// export function signUpUser(user) {
-//   return () => axios.post('/auth/signUp', user)
-//   .then((response) => {
-//     console.log(response);
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
-// }
-
-// export function errorHandler(dispatch, error, type) {
-//   let errorMessage = '';
-
-//   if (error.data.error) {
-//     errorMessage = error.data.error;
-//   } else if (error.data) {
-//     errorMessage = error.data;
-//   } else {
-//     errorMessage = error;
-//   }
-
-//   if (error.status === 401) {
-//     dispatch({
-//       type,
-//       payload: `You are not authorized to do this.
-//                 Please login and try again.`,
-//     });
-//     logoutUser();
-//   } else {
-//     dispatch({
-//       type,
-//       payload: errorMessage,
-//     });
-//   }
-// }
-
-// export function loginUser({ email, password }) {
-//   return function login(dispatch) {
-//     axios.post(`${API_URL}/auth/login`, { email, password })
-//     .then((response) => {
-//       cookie.save('token', response.data.token, { path: '/' });
-//       dispatch({ type: AUTH_USER });
-//       // window.location.href = CLIENT_ROOT_URL + '/dashboard';
-//     })
-//     .catch((error) => {
-//       errorHandler(dispatch, error.response, AUTH_ERROR);
-//     });
-//   };
-// }
-
-// export function registerUser({ email, firstName, lastName, password }) {
-//   return function(dispatch) {
-//     axios.post(`${API_URL}/auth/register`, { email, firstName, lastName, password })
-//     .then(response => {
-//       cookie.save('token', response.data.token, { path: '/' });
-//       dispatch({ type: AUTH_USER });
-//       window.location.href = CLIENT_ROOT_URL + '/dashboard';
-//     })
-//     .catch((error) => {
-//       errorHandler(dispatch, error.response, AUTH_ERROR)
-//     });
-//   }
-// }
-
-// export function protectedTest() {
-//   return function(dispatch) {
-//     axios.get(`${API_URL}/protected`, {
-//       headers: { 'Authorization': cookie.load('token') }
-//     })
-//     .then(response => {
-//       dispatch({
-//         type: PROTECTED_TEST,
-//         payload: response.data.content
-//       });
-//     })
-//     .catch((error) => {
-//       errorHandler(dispatch, error.response, AUTH_ERROR)
-//     });
-//   }
-// }
--- client/app/bundles/HelloWorld/reducers/AuthReducer.jsx
+++ client/app/bundles/HelloWorld/reducers/AuthReducer.jsx
@@ -1,39 +1,8 @@
-import { AUTH_USER,
-         UNAUTH_USER,
-         AUTH_ERROR,
-         PROTECTED_TEST,
-         SIGN_UP_USER_FULFILLED } from '../constants/authConstants';
-
 const INITIAL_STATE = {
   isAuthenticating: false,
   currentUser: null,
   errorMessage: null,
 };
-
-// const authReducer = (state = INITIAL_STATE, action) => {
-//   switch (action.type) {
-//     case AUTH_USER:
-//       return {
-//         ...state,
-//         error: '',
-//         message: '',
-//         authenticated: true,
-//       };
-//     case UNAUTH_USER:
-//       return { ...state, authenticated: false };
-//     case AUTH_ERROR:
-//       return { ...state, error: action.payload };
-//     case PROTECTED_TEST:
-//       return { ...state, content: action.payload };
-//     case SIGN_UP_USER_FULFILLED:
-//       console.log('signup successful');
-//       return { ...state };
-//     default:
-//       return { ...state };
-//   }
-
-//   return state;
-// };
 
 const authReducer = (state = INITIAL_STATE, action = {}) => {
   switch (action.type) {
